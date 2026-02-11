77c4efae55d70d9ce20bd7792223e322fb84b497
Durodola Abolaji <bolajidurodola@gmail.com>
2026-02-11 21:03:25 +0100

    update

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,7 +4,13 @@ import './App.css'
 function App() {
   return (
     <>
-    <h1>Hi, I'm Durodola Abolaji Toliat. I am a Software Engineer</h1>
+    <h1>
+      My name is Durodola Abolaji Toliat. As a Software Engineer, 
+      I treat programming languages as a means to an end. 
+      My foundation is in Java, but my strength lies in my adaptability;
+       I can pick up new stacks rapidly, ensuring that technology choices 
+       never limit the solutions I build."
+    </h1>
     </>
   )
 }
